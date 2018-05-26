32a619c1996b2750a590f34bff099e8c59238f5d
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-05-25 22:57:14 -0300

    Improving Cache

--- src/main/java/br/com/caelum/fj91/microservices/AuthorsBoot.java
+++ src/main/java/br/com/caelum/fj91/microservices/AuthorsBoot.java
@@ -2,7 +2,9 @@ package br.com.caelum.fj91.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class AuthorsBoot {
 
--- src/main/java/br/com/caelum/fj91/microservices/controllers/AuthorsController.java
+++ src/main/java/br/com/caelum/fj91/microservices/controllers/AuthorsController.java
@@ -4,6 +4,8 @@ import br.com.caelum.fj91.microservices.exceptions.NotFoundException;
 import br.com.caelum.fj91.microservices.forms.AuthorForm;
 import br.com.caelum.fj91.microservices.models.Author;
 import br.com.caelum.fj91.microservices.repositories.Authors;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,15 +26,18 @@ public class AuthorsController {
     }
 
     @GetMapping
+    @Cacheable("authors")
     public List<Author> list(){
         return authors.findAll();
     }
 
     @GetMapping("{id}")
+    @Cacheable("author")
     public Author show(@PathVariable Long id){
         return authors.findById(id).orElseThrow(NotFoundException::new);
     }
 
+    @CacheEvict(value = {"authors","author"},allEntries = true)
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Author> save(@RequestBody @Valid AuthorForm form){
         Author author = form.toEntity();
